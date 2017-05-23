02fbf75f8e804c404cbbf249badb846b24991e19
amalshehu <amalshehu@gmail.com>
2017-05-23 13:14:47 +0530

    Add ngx translate

--- src/client/app/home/home.component.ts
+++ src/client/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NameListService } from '../shared/name-list/name-list.service';
-
+import { TranslateService } from '@ngx-translate/core';
 /**
  * This class represents the lazy loaded HomeComponent.
  */
@@ -15,14 +15,19 @@ export class HomeComponent implements OnInit {
   newName: string = '';
   errorMessage: string;
   names: any[] = [];
-
   /**
    * Creates an instance of the HomeComponent with the injected
    * NameListService.
    *
    * @param {NameListService} nameListService - The injected NameListService.
    */
-  constructor(public nameListService: NameListService) {}
+  constructor(public nameListService: NameListService, public translate: TranslateService) {
+     translate.addLangs(['en', 'fr']);
+     translate.setDefaultLang('en');
+
+      let browserLang = translate.getBrowserLang();
+      translate.use(browserLang.match(/en|fr/) ? browserLang : 'en');
+  }
 
   /**
    * Get the names OnInit
--- src/client/app/home/home.module.ts
+++ src/client/app/home/home.module.ts
@@ -3,11 +3,30 @@ import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { NameListService } from '../shared/name-list/name-list.service';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateService } from '@ngx-translate/core';
+import { HttpModule, Http } from '@angular/http';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: Http) {
+    return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
+}
 
 @NgModule({
-  imports: [HomeRoutingModule, SharedModule],
+  imports: [HomeRoutingModule,
+            SharedModule,
+             HttpModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [Http]
+          }
+        })
+  ],
   declarations: [HomeComponent],
   exports: [HomeComponent],
-  providers: [NameListService]
+  providers: [NameListService, TranslateService]
 })
 export class HomeModule { }
